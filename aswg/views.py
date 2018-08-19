98dc96b130b708c858c5bd426ef9afb99794f52b
allisnone <zgx20022002@163.com>
2018-08-19 13:08:06 +0800

    project to initial security preview

---
+++ aswg/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render,redirect
+from . import models
+from aiohttp.client import request
+# Create your views here.
+def get_classes(request):
+    portlists=models.chkportinfo.objects.all()
+    return render(request,'get_classes.html',{'portlists':portlists})
+ 
+def add_classes(request):
+    if request.method == "GET":
+        return render(request, 'add_classes.html')
+    elif request.method == 'POST':
+        ips = request.POST.get('ips')
+        ports=request.POST.get('ports')
+        contacts=request.POST.get('contacts')
+        models.chkportinfo.objects.create(IPs=ips,ports=ports,contact=contacts)
+        return redirect('/get_classes.html')
+ 
+ 
+def del_classes(request):
+    id = request.GET.get('id')
+    models.chkportinfo.objects.filter(id=id).delete()
+    return redirect('/get_classes.html')
+ 
+def edit_classes(request):
+    if request.method == 'GET':
+        id = request.GET.get('id')
+        obj = models.chkportinfo.objects.filter(id=id).first()
+        return render(request, 'edit_classes.html', {'obj': obj})
+    elif request.method == 'POST':
+        id = request.GET.get('id')
+        ips = request.POST.get('ips')
+        ports = request.POST.get('ports')
+        contacts=request.POST.get('contacts')
+        models.chkportinfo.objects.filter(id=id).update(IPs=ips,ports=ports,contact=contacts)
+        return redirect('/get_classes.html')
+
+def login(request):
+    return redirect('/login.html')
+
+def index(request):
+    return redirect('/index.html')
---
+++ skyguard/urls.py
@@ -0,0 +1,62 @@
+"""skyguard URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf.urls import include, url#,patterns
+from django.contrib import admin
+from django.urls import path
+#import aswg.views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    url(r'', include('aswg.urls')),
+]
+
+
+"""
+
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^app1/login/', views.login),
+    url(r'^app1/index/', views.index),
+    url(r'^get_classes.html', views.get_classes),
+    url(r'^add_classes.html', views.add_classes),
+    url(r'^del_classes.html', views.del_classes),
+    url(r'^edit_classes.html', views.edit_classes),
+]
+"""
+
+"""
+
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'EMS.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('user.urls')),
+    url(r'^', include('dashboard.urls')),
+    url(r'^', include('report.urls')),
+    url(r'^', include('config.urls')),
+    url(r'^', include('about.urls')),
+    url(r'^', include('load.urls')),
+#    url(r'^', include('old_dashboard.urls')),
+)
+"""
