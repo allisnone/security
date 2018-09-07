1c42a8c4fc9feb3af45ec4cc04c890c08f539641
allisnone <zgx20022002@163.com>
2018-09-07 17:46:50 +0800

    up

--- static/js/aswg.js
+++ static/js/aswg.js
@@ -287,7 +287,7 @@ function xmlhttp(id,rawData,statusImg) {
   var resultStatus = 1;
   var xhr = getHttpObj();
   //xhr.open("POST", rawData.urls, true);
-  xhr.open(rawData.method, rawData.urls, true);
+  xhr.open(rawData.method, rawData.urls, false);
   //alert(url+'--test: ' + content);
   if (rawData.method =="post" || rawData.method =="POST"){
 	  xhr.responseType = "text"; //json,document, arraybuffer
