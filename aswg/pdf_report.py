7d63df6147eddc53cf5865276a26fe33aa2f4577
allisnone <zgx20022002@163.com>
2018-09-12 18:34:28 +0800

    up

--- aswg/pdf_report.py
+++ aswg/pdf_report.py
@@ -3,7 +3,11 @@
 #https://pypi.org/project/pdfkit/
 #https://www.cnblogs.com/my8100/p/7738366.html
 import pdfkit
-url = 'http://114.116.87.228:8001/json'
+url = 'http://114.116.87.228:8001/jsonp'
 #url = 'www.baidu.com'
 #url = 'https://ask.csdn.net/questions/387309'
-pk = pdfkit.from_url(url,'out.pdf')
+url ='https://www.cnblogs.com/niejinmei/p/8157680.html'
+options = { 'page-size': 'A4', 'margin-top': '0mm', 
+           'margin-right': '0mm', 'margin-bottom': '0mm', 'margin-left': '0mm'}# 'orientation':'Landscape',#横向 'encoding': "UTF-8", 'no-outline': None, # 'footer-right':'[page]' 设置页码 }
+
+pk = pdfkit.from_url(url,'out.pdf',options=options)
